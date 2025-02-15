a16bd15235aa285c8faed2c8c28004a45692835b
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2025-02-16 00:03:26 +0100

    test block

--- Tetris/BlockTemplates.cs
+++ Tetris/BlockTemplates.cs
@@ -4,6 +4,15 @@ public static class BlockTemplates
 {
     public static List<Tile> GetFromType(BlockType blockType)
     {
-        return [];
+        return blockType switch
+        {
+            BlockType.Ka => Ka(),
+            _ => throw new ArgumentOutOfRangeException(nameof(blockType), blockType, null)
+        };
+    }
+
+    private static List<Tile> Ka()
+    {
+        return [new Tile(Board.Width / 2, Board.Height + 1)];
     }
 }
