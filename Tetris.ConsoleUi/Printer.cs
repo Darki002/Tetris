65d28fa51fd67cdb4035d06d0ac800df321c1ca8
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2025-02-16 00:24:01 +0100

    Update Printer.cs

--- Tetris.ConsoleUi/Printer.cs
+++ Tetris.ConsoleUi/Printer.cs
@@ -5,12 +5,16 @@ public class Printer : IPrinter
     public void Print(Board board, Block currentBlock)
     {
         Console.WriteLine("------------------Print------------------");
-        foreach (var tile in board.Tiles)
+        
+        Console.WriteLine("Current Block:");
+        foreach (var tile in currentBlock.Tiles)
         {
             Console.WriteLine($"X: {tile.X} Y: {tile.Y}");
         }
-
-        foreach (var tile in currentBlock.Tiles)
+        
+        Console.WriteLine("Board:");
+        
+        foreach (var tile in board.Tiles)
         {
             Console.WriteLine($"X: {tile.X} Y: {tile.Y}");
         }
