0647c1caed3a0e6ba015be53376d25758b536dd3
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2025-02-16 00:33:14 +0100

    blcok types to test

--- Tetris/BlockTemplates.cs
+++ Tetris/BlockTemplates.cs
@@ -12,9 +12,21 @@ public static class BlockTemplates
         return blockType switch
         {
             BlockType.Test => Test(),
+            BlockType.TestWidth => TestWidth(),
+            BlockType.TestHeight => TestHeight(),
             _ => throw new ArgumentOutOfRangeException(nameof(blockType), blockType, null)
         };
     }
+
+    private static Block TestHeight() => new Block([
+        new Tile(Board.Width / 2, Board.Height + 1),
+        new Tile(Board.Width / 2, Board.Height + 2)
+    ]);
+
+    private static Block TestWidth() => new Block([
+        new Tile(Board.Width / 2, Board.Height + 1),
+        new Tile(Board.Width / 2 + 1, Board.Height + 1)
+    ]);
 
     private static Block Test()
     {
--- Tetris/BlockType.cs
+++ Tetris/BlockType.cs
@@ -2,5 +2,7 @@
 
 public enum BlockType
 {
-    Test
+    Test,
+    TestWidth,
+    TestHeight
 }
