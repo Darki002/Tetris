4ea4d161294dc076a185b729da38ed53a6c74bf7
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2025-02-16 11:39:04 +0100

    web project

---
+++ Tetris.Web/Components/Pages/Index.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Tetris.Web.Components.Pages;
+
+public partial class Index(NavigationManager navigationManager)
+{
+    private void OnStart()
+    {
+        navigationManager.NavigateTo("game");
+    }
+}
---
+++ Tetris.Web/Components/Pages/TetrisGame.razor.cs
@@ -0,0 +1,25 @@
+﻿namespace Tetris.Web.Components.Pages;
+
+public partial class TetrisGame : IPrinter
+{
+    private Game game = null!;
+
+    private List<Tile> tiles = [];
+
+    protected override void OnInitialized()
+    {
+        game = new Game(this);
+        game.Start();
+    }
+
+    protected override void OnAfterRender(bool firstRender)
+    {
+        
+    }
+
+    public void Print(Board board, Block currentBlock)
+    {
+        tiles = [..board.Tiles, ..currentBlock.Tiles];
+        StateHasChanged();
+    }
+}
